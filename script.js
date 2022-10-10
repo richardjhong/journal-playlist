c8e41078b162fb2079476728319de7c03e7c6302
Brandon Knight <knightbrandon1984@gmail.com>
2022-10-10 14:26:07 -0500

    Added the Emotional API

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// Emotional Analysis API
+const userContainer = document.getElementById('users')
+const button = document.getElementById('fetch-button')
+const textArea = document.getElementById('textarea')
+const contentContainer = document.getElementById('container')
 
+contentContainer.addEventListener("click", function(event){
+
+let currentText = textArea.value
+if (event.target.id === 'fetch-button'){
+    console.log(currentText)
+grabStrongestEmotion(currentText);
+}
+
+
+})
+
+
+
+// // //API Emotional Damage
 const options = {
 	method: 'GET',
 	headers: {
@@ -8,7 +25,52 @@ const options = {
 	}
 };
 
-fetch('https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=After%20living%20abroad%20for%20such%20a%20long%20time%2C%20seeing%20my%20family%20was%20the%20best%20present%20I%20could%20have%20ever%20wished%20for.', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+function grabEmotions(textInput) {
+    return fetch('https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=' + textInput, options)
+      .then(response => response.json())
+      .then(response => {return response.emotion_scores})
+      .catch(err => console.error(err));
+  }
+  
+  async function grabStrongestEmotion(textInput) {
+    let emotionScores = await grabEmotions(textInput);
+    console.log('emotionScores: ', emotionScores);
+    let scores = Object.values(emotionScores);
+    let maxScore = Math.max(...scores)
+    console.log('maxScore: ', maxScore);
+    let strongestEmotion = Object.keys(emotionScores).filter(key => emotionScores[key] === maxScore)
+    console.log(strongestEmotion); 
+  }
+
+    
+//     function grabEmotions(textInput) {
+//         return fetch('https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=After%20living%20abroad%20for%20such%20a%20long%20time%2C%20seeing%20my%20family%20was%20the%20best%20present%20I%20could%20have%20ever%20wished%20for.', options)
+//         .then(response => response.json())
+//         .then(response => {
+//             // console.log(response.emotion_scores.joy)
+//         // let emotionScores = ['anger', 'disgust', 'fear', 'sadness', 'surprise', 'joy'];
+//         // Math.max(emotionScores);
+//         // console.log(response.emotion_scores.Math.max());
+//     // let maxScore = Object.values(response.emotion_scores);
+//     return response.emotion_scores;
+//     console.log(Math.max(maxScore));
+        
+    
+//     })
+    
+//         .catch(err => console.error(err))
+//     }
+
+//     async function grabStrongestEmotion() {
+// let emotionScores = await grabEmotions();
+// let maxScore = Math.max(Object.values(emotionScores));
+// console.log(maxScore);
+// let strongestEmotion = Object.keys(emotionScores).filter(key => emotionScores[key] === maxScore)
+// console.log(strongestEmotion); 
+
+//     }
+
+
+
+
+
