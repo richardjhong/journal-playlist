31e902643704dbf12cf2ce82b6953dd47a8845c4
Richard Hong <rhong24@gmail.com>
2022-10-13 12:00:48 -0500

    modifying snackbar notifcation to display different messages based on whether clicking log journal with an empty input or if user is fetching a playlist and giving a visual indicator of what emotion correlates to the playlist

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,7 @@ skeletonCard.appendChild(skeletonHeader)
 skeletonCard.appendChild(skeletonImage)
 
 // needs individual API keys in line below
-var apiKey = '2ece9e8550msh088597d0eac2b3fp18c132jsn07f0c44b7eb5'
+var apiKey = 'INSERT API KEY HERE'
 
 const options = {
   SpotifyAPI: {
@@ -89,7 +89,7 @@ inputContainerEl.addEventListener('click', function(e) {
     playListContainerEl.prepend(skelClone)
     injectPlaylistContainer(textAreaInput)
   } else if (e.target.id === 'fetch-button' && textArea.value.length === 0) {
-    showSnackBarNotification()
+    showSnackBarNotification('emptyText')
   }
 })
 
@@ -98,11 +98,24 @@ if (localStorage.getItem("emotionCollection")) {
   document.getElementById('chart-modal-button').style.display = "block"
 }
 
-function showSnackBarNotification() {
-  var snackBar = document.getElementById("snackbar");
-  snackBar.className = "show";
 
-  setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+
+function showSnackBarNotification(type, emotion = null) {
+  var snackBar = document.getElementById("snackbar");
+  
+  if (type === 'emptyText') {
+    snackBar.innerText = 'You must input a journal entry to evaluate before generating a playlist!'
+    snackBar.className = "show";
+    setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 3000);
+  }
+
+  // gives user insight on how the textInput correlates to the playlist being 
+  // retrieved
+  if (type === 'emotionSearchQuery') {
+    snackBar.innerText = `I think that you are feeling ${emotion}` 
+    snackBar.className = "show";
+    setTimeout(function(){ snackBar.className = snackBar.className.replace("show", ""); }, 5000);
+  }
 }
 
 
@@ -132,6 +145,7 @@ async function grabStrongestEmotion(textInput) {
   localStorage.setItem("emotionCollection", JSON.stringify(emotionCollection))
 
   updateChart()
+  showSnackBarNotification("emotionSearchQuery", strongestEmotion[0])
 
   // Bulma modal boilerplate code adds evenListeners based on 
   // 'DOMContentLoaded'. To not disturb its behavior, chart-modal-button
